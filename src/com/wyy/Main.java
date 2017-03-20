000a13b265bdd319d690c9f14f14a3af3175fd7d
wyy <wyy20188@126.com>
2017-03-20 20:01:24 +0800

    test for input method

--- src/com/wyy/Main.java
+++ src/com/wyy/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        RoomMap roomMap=new RoomMap("E://CCPP/maze.txt");
+        roomMap.printRoomTrace(roomMap.theMap);
+        System.out.println();
     }
 }
